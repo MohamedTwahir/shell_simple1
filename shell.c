82080d4c530f17eef93829c82890eff04b9a8302
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-09-19 18:44:27 -0700

    added guards to header file" "

---
+++ shell.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+int main(int ac, char **argv)
+{
+	char *prompt = "#cisfun$ ";
+	char *get_line;
+	size_t n = 0;
+
+	(void)ac; (void)argv;
+
+	printf("%s", prompt);
+	getline(&get_line, &n, stdin);
+	printf("%s\n", get_line);
+
+	free(get_line);
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/*
+ * File: shell header file
+ * Author: Mohamed Twahir
+ *
+ */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#endif
