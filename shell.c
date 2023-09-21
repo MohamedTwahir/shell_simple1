bf71e3c0be62dd863e0c1a2ce58bbb405449a479
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-09-20 19:32:42 -0500

    handle end of file

--- shell.c
+++ shell.c
@@ -7,15 +7,32 @@
 int main(int ac, char **argv)
 {
 	char *prompt = "#cisfun$ ";
-	char *get_line; //stores the address of the buffer holding whatever was typed
+	char *get_line = NULL; //stores the address of the buffer holding whatever was typed
 	size_t n = 0;
+	ssize_t reader;
+
+	get_line = (char *)malloc(20);
 
 	(void)ac; (void)argv;
 
-	write(STDOUT_FILENO, prompt, strlen(prompt));
-	getline(&get_line, &n, stdin);
-	write(STDOUT_FILENO, get_line, strlen(get_line));
+	while (1){
+		write(STDOUT_FILENO, prompt, strlen(prompt));
+		reader = getline(&get_line, &n, stdin);
+		// write(STDOUT_FILENO, get_line, strlen(get_line));
+		if (reader == -1){
+			// perror("Exiting shell...\n");
+			return (-1);
+		}
 
-	free(get_line);
+		write(STDOUT_FILENO, get_line, strlen(get_line));
+
+		free(get_line);
+	}
+	// write(STDOUT_FILENO, prompt, strlen(prompt));
+	// getline(&get_line, &n, stdin);
+	// write(STDOUT_FILENO, get_line, strlen(get_line));
+
+	// free(get_line);
+   
 	return (0);
 }
